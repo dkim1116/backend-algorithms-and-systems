8478160b20d5342c5e6ee6c8a910a84654d6e558
Daniel Kim <daniel.kim1116@gmail.com>
2026-03-30 11:27:34 -0700

    finished sorted stream merge

--- algorithms/py/_review/review/case23_sorted_stream_merge.py
+++ algorithms/py/_review/review/case23_sorted_stream_merge.py
@@ -15,7 +15,26 @@ class ListNode:
         self.val = val
         self.next = next
 
+import heapq
 
 class Solution:
     def solve(self, lists: list[ListNode | None]) -> ListNode | None:
-        pass
+        minHeap = []
+
+        for i, node in enumerate(lists):
+            if node is not None:
+                heapq.heappush(minHeap, (node.val, i, node))
+
+        dummyNode = ListNode(0)
+        tail = dummyNode
+
+        while minHeap:
+            val, i, node = heapq.heappop(minHeap)
+
+            tail.next = node
+            tail = node
+
+            if node.next:
+                heapq.heappush(minHeap, (node.next.val, i, node))
+
+        return dummyNode.next
