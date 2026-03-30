b20a1148fe2db2187963793cdf6b8695b7c5a336
Daniel Kim <daniel.kim1116@gmail.com>
2026-03-30 11:03:00 -0700

    finished courseSchedule variant

--- algorithms/py/_review/review/case18_dependency_finish.py
+++ algorithms/py/_review/review/case18_dependency_finish.py
@@ -11,6 +11,71 @@ from __future__ import annotations
 # Input: numCourses = 2, prerequisites = [[1, 0], [0, 1]]
 # Output: False
 
-class Solution:
+class SolutionDFS:
     def solve(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
-        pass
+        courseMap = {}
+
+        for course, prereq in prerequisites:
+            courseMap[course] = courseMap.get(course, [])
+            courseMap[course].append(prereq)
+
+        visited = set()
+        visiting = set()
+
+        def dfs(course):
+            if course in visited:
+                return True
+            
+            if course in visiting:
+                return False
+            
+            visiting.add(course)
+
+            for prereq in courseMap[course]:
+                if dfs(prereq) == False:
+                    return False
+                
+            visiting.remove(course)
+            visited.add(course)
+
+            return True
+        
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return False
+            
+        return True
+
+class SolutionBFS:
+    def solve(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        courseMap = {}
+        prereqFreqBucket = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            courseMap[prereq] = courseMap.get(prereq, [])
+            courseMap[prereq].append(course)
+            prereqFreqBucket[course] += 1
+
+        queue = []
+        queuePointer = 0
+
+        for prereq in range(len(prereqFreqBucket)):
+            if prereqFreqBucket[prereq] == 0:
+                queue.append(prereq)
+
+        courseCompleted = 0
+
+        while queuePointer < len(queue):
+            prereq = queue[queuePointer]
+            queuePointer += 1
+
+            courseCompleted += 1
+
+            for course in courseMap.get(prereq, []):
+                prereqFreqBucket[course] -= 1
+
+                if prereqFreqBucket[course] == 0:
+                    queue.append(course)
+        
+        return courseCompleted == numCourses
+            
