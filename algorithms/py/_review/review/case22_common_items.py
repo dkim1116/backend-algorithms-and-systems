1d4940369264f08323f324315ca21924bc4df31b
Daniel Kim <daniel.kim1116@gmail.com>
2026-03-30 11:18:44 -0700

    finished common items

--- algorithms/py/_review/review/case22_common_items.py
+++ algorithms/py/_review/review/case22_common_items.py
@@ -12,4 +12,22 @@ from __future__ import annotations
 
 class Solution:
     def solve(self, nums: list[int], k: int) -> list[int]:
-        pass
+        freqMap = {}
+
+        for num in nums:
+            freqMap[num] = freqMap.get(num, 0)
+            freqMap[num] += 1
+
+        freqBucket = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in freqMap.items():
+            freqBucket[freq].append(num)
+
+        res = []
+
+        for i in reversed(range(len(nums))):
+            for num in freqBucket[i]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
